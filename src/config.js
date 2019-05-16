dc7def1386aa96ceb3e400e79240e8492b448a8f
john sanford <Jack@Macintosh.local>
2019-05-15 22:39:21 -0700

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const dev = {
+  STRIPE_KEY: "pk_test_Pd7AqcCG8Or017EospWaUDXp004D5MLWq9",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-dev-attachmentsbucket-10xc7qtcc3i6t"
@@ -13,10 +14,10 @@ const dev = {
     APP_CLIENT_ID: "35d9hocqtu1k19ad4bej0k016a",
     IDENTITY_POOL_ID: "us-east-1:e24f3129-200d-4767-8f02-036df5deb5d7"
   }
-  STRIPE_KEY: "pk_test_Pd7AqcCG8Or017EospWaUDXp004D5MLWq9",
 };
 
 const prod = {
+  STRIPE_KEY: "pk_test_Pd7AqcCG8Or017EospWaUDXp004D5MLWq9",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-1mcowzhm79vkb"
@@ -31,7 +32,6 @@ const prod = {
     APP_CLIENT_ID: "2q5ulkmc5q1kr13u58niklba73",
     IDENTITY_POOL_ID: "us-east-1:a88370d9-53f8-4c11-8de1-788a6b797ded"
   }
-  STRIPE_KEY: "pk_test_Pd7AqcCG8Or017EospWaUDXp004D5MLWq9",
 };
 
 // Default to dev if not set
